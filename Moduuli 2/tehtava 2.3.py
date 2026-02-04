f138b9aa2f5f436ea78e0069927eb2062830b2f6
Anniina <anniina.marttinen@metropolia.fi>
2026-02-04 11:48:47 +0200

    väli tallennus

--- Moduuli 2/tehtava 2.3.py
+++ Moduuli 2/tehtava 2.3.py
@@ -1,2 +1,11 @@
-"Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden. Ohjelma tulostaa suorakulmion piirin ja pinta-alan."
+"Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden." \
+"Ohjelma tulostaa suorakulmion piirin ja pinta-alan."
+
+import math
+kanta = float(input('Mikä on suorakulmion kanta?:'))
+korkeus = float(input('Mikä on suorakulmion korkeus?:'))
+piiri = (kanta*2)+(korkeus)*2
+pinta_ala = kanta*korkeus
+
+print("Suorakulmiosi "+str(kanta)+" x "+str(korkeus)+" piiri on "+str(piiri)+"ja pinta-ala on "+str(pinta_ala))
 
---
+++ Moduuli 2/tehtava 2.4.py
@@ -0,0 +1,14 @@
+"Kirjoita ohjelma, joka kysyy kolme kokonaislukua."
+"Ohjelma tulostaa lukujen summan, tulon ja keskiarvon."
+
+print("Anna yhteensä kolme kokonaislukua 0 ja 100 välillä")
+
+numero1 = int(input('Ensimmäinen luku:'))
+numero2 = int(input('Toinen luku:'))
+numero3 = int(input('Kolmas luku:'))
+
+summa = numero1 + numero2 + numero3
+tulo = numero1 * numero2 * numero3
+keskiarvo = (numero1 + numero2 + numero3) /3
+
+print("Lukujesi summa on " + str(summa)+ ", tulo on " + str(tulo)+ ", keskiarvo on " +str(keskiarvo))
